d62edd17a02882b024d43c4403db07ec3a5d453d
Mohini Satarupa Jena <mohinisatarupajena@gmail.com>
2026-07-01 07:34:48 +0530

    Add: duplicate-integer - submission-1

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.py
@@ -0,0 +1,18 @@
+class Solution:
+    #brute force t:o(n^2) s:o(n)
+    # def hasDuplicate(self, nums: List[int]) -> bool:
+    #     n=len(nums)
+    #     for i in range(n):
+    #         for j in range(i+1,n):
+    #             if nums[i]==nums[j]:
+    #                 return True
+    #     return False    
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        seen=set()
+        for i in range(len(nums)):
+            if nums[i] in seen:
+                return True
+            else:
+                seen.add(nums[i])
+        return False
+
